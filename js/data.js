c363c7b165a532584044e04da4957a835802d457
srihari kapu <sriharikapu163@gmail.com>
2020-07-07 22:20:58 +0800

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,53 @@
+/* Model Data */
+
+var locations = [
+    {
+        title: 'Quil City Mall',
+        location: {
+            lat: 3.1599,
+            lng: 101.7001
+        }
+    },
+    {
+        title: 'LuLu Kula Lumpur', 
+        location: {
+            lat: 3.1546,
+            lng: 101.6998
+        }
+    },
+    {
+        title: 'Plaza Lowyat',
+        location: {
+            lat: 3.1441,
+            lng: 101.7100
+        }
+    },
+    {
+        title: 'Petaling Street',
+        location: {
+            lat: 3.1427,
+            lng: 101.6979
+        }
+    },
+    {
+        title: 'Great Eastren Mall',
+        location: {
+            lat: 3.1601,
+            lng: 101.7369
+        }
+    },
+    {
+        title: 'Suria KLCC',
+        location: {
+            lat: 3.1580,
+            lng: 101.7117
+        }
+    },
+    {
+        title: 'Pudu',
+        location: {
+            lat: 3.1348,
+            lng: 101.7136
+        }
+    }
+];
