fc6ec208ec741ed7c73ecd598c1eeda8be06d040
srihari kapu <sriharikapu163@gmail.com>
2020-07-07 22:43:08 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -4,50 +4,50 @@ var locations = [
     {
         title: 'Quil City Mall',
         location: {
-            lat: 3.1599,
-            lng: 101.7001
+            lat: 3.1599018,
+            lng: 101.7001124
         }
     },
     {
         title: 'LuLu Kula Lumpur', 
         location: {
-            lat: 3.1546,
-            lng: 101.6998
+            lat: 3.1546985,
+            lng: 101.6998123
         }
     },
     {
         title: 'Plaza Lowyat',
         location: {
-            lat: 3.1441,
-            lng: 101.7100
+            lat: 3.1441775,
+            lng: 101.7100165
         }
     },
     {
         title: 'Petaling Street',
         location: {
-            lat: 3.1427,
-            lng: 101.6979
+            lat: 3.1427102,
+            lng: 101.6979321
         }
     },
     {
         title: 'Great Eastren Mall',
         location: {
-            lat: 3.1601,
-            lng: 101.7369
+            lat: 3.1601100,
+            lng: 101.7369012
         }
     },
     {
         title: 'Suria KLCC',
         location: {
-            lat: 3.1580,
-            lng: 101.7117
+            lat: 3.1580801,
+            lng: 101.7117550
         }
     },
     {
         title: 'Pudu',
         location: {
-            lat: 3.1348,
-            lng: 101.7136
+            lat: 3.1348070,
+            lng: 101.7136011
         }
     }
 ];
